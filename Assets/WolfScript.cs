8621274202389015afbd0ae30ecceaa265f01481
Shiraz V Kamaruddin <shrvk@gmail.com>
2021-01-29 15:11:02 +0530

    Animation and Model stuff 1

---
+++ Assets/WolfScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.AI;
+
+public class WolfScript : MonoBehaviour
+{
+    public enum State { Idle, triggered, stay, chase, kill, Retreat}
+
+    public NavMeshAgent enemyagent ;
+    public State state = default;
+
+    public bool isOutOfRange;
+    public bool isInRange;
+    public bool Retreat;
+
+}
